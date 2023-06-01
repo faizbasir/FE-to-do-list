335cf39024e7f148251a2acdae14f4a94894b2a6
Faiz Basir <faizbasir@Faizs-MacBook-Pro.local>
2023-06-01 23:32:17 +0800

    started on calendar function

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Header from "./components/UI/header/Header";
 import TaskList from "./components/UI/content/tasks/TaskList";
+import CalendarList from "./components/UI/content/calendar/CalendarList";
 
 const App = () => {
   const routes = (
     <Routes>
       <Route path="/" element={<TaskList />} />
       <Route path="/overview" element={<TaskList />} />
+      <Route path="/calendar" element={<CalendarList />} />
     </Routes>
   );
 
