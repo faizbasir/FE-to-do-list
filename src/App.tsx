5017794e557d1f125e8ed3a87b5915dc0f8a1549
Faiz Basir <faizbasir@Faizs-MacBook-Pro.local>
2023-08-27 18:28:59 +0800

    updated ui with ag grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Header from "./components/UI/header/Header";
 import TaskList from "./components/UI/content/tasks/TaskList";
 import CalendarList from "./components/UI/content/calendar/CalendarList";
 import "./App.scss";
+import Grid from "./components/UI/content/grid/Grid";
 
 const App = () => {
   const routes = (
     <Routes>
       <Route path="/" element={<TaskList />} />
-      <Route path="/overview" element={<TaskList />} />
+      {/* <Route path="/overview" element={<TaskList />} /> */}
       <Route path="/calendar" element={<CalendarList />} />
+      <Route path="/overview" element={<Grid />} />
     </Routes>
   );
 
---
+++ src/components/UI/content/grid/Grid.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from "react";
+import { AgGridReact } from "@ag-grid-community/react";
+import { ColDef, ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import "@ag-grid-community/styles/ag-grid.css";
+import "@ag-grid-community/styles/ag-theme-alpine.css";
+import "./styles/Grid.scss";
+import { useAppDispatch, useAppSelector } from "../../../store/store";
+import { fetchTasks } from "../../../store/todoSlice";
+
+interface task {
+  id: number;
+  summary: string;
+  completed: boolean;
+  dueDate: string;
+}
+
+const Grid = () => {
+  ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+  const appDispatch = useAppDispatch();
+  const taskList = useAppSelector((state) => state.todo.tasks);
+  const gridRef = useRef();
+  const [rowData, setRowData] = useState<task[]>([]);
+  const [columnDefs, setColumnDefs] = useState<ColDef[]>([
+    { field: "id" },
+    { field: "summary" },
+    { field: "completed" },
+    { field: "dueDate" },
+  ]);
+
+  useEffect(() => {
+    appDispatch(fetchTasks());
+  }, []);
+
+  useEffect(() => {
+    setRowData(taskList);
+  }, [taskList]);
+
+  return (
+    <>
+      <div className="ag-theme-alpine">
+        <AgGridReact columnDefs={columnDefs} rowData={rowData} />
+      </div>
+    </>
+  );
+};
+
+export default Grid;
